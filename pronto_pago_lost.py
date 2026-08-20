8387fb6261226f872a85205f67a724698b40c232
abrahamvelazquezvelazquez <abraham.velazquezvelazquez@mercadolibre.com.mx>
2026-08-20 14:49:13 -0600

    Update main sheet ID for pronto_pago_lost.py

--- pronto_pago_lost.py
+++ pronto_pago_lost.py
@@ -7,7 +7,7 @@ creds = Credentials.from_service_account_file("credentials.json", scopes=SCOPES)
 client = gspread.authorize(creds)
 
 # Configuración de Hoja Principal
-ID_HOJA_PRINCIPAL = "1pNY27z4TuxzqvUHdjxKidO29sbJt5tBZfQDNvBOLh4o"
+ID_HOJA_PRINCIPAL = "1tLAyayZkAWJ0XtyQWWILutdQ_8sr7rjf1VsXxcAuL4M"
 NOMBRE_PESTAÑA_PRINCIPAL = "Seguimiento"
 
 # Hoja externa para pronto_pago
